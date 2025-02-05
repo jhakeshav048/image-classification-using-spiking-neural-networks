e226421b0ecbb70bbb1fa849a3c50fa7b528ba28
Keshav <keshav@distronix.in>
2025-02-05 08:53:00 +0530

    Created initialize file for the project

---
+++ image_classification_using_spiking_neural_networks/__init__.py
@@ -0,0 +1,9 @@
+# my_project/__init__.py
+"""
+spiking-neural-networks: A Python package for Image Classification using Spiking Neural Networks.
+"""
+
+# Import key classes/functions for easier access
+from .snn import LIFNeuron, Layer, SpikingNeuralNetwork
+
+__version__ = "0.1.0"
